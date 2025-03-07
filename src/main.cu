d6814aacd52b3043003b6c5831e72ce161a15e63
stevehenny <stephen.henstrom@gmail.com>
2025-03-06 21:28:28 -0700

    refactoring and working on project organization

--- src/main.cu
+++ src/main.cu
@@ -2,10 +2,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  const char *imageFile =
-      "../dataset/train-images-idx3-ubyte/train-images-idx3-ubyte";
-  const char *labelFile =
-      "../dataset/train-labels-idx1-ubyte/train-labels-idx1-ubyte";
+  if (argc != 3) {
+    fprintf(stderr, "Usage: <imageFile> <labels>");
+    return 1;
+  }
+
+  const char *imageFile = argv[1];
+  const char *labelFile = argv[2];
 
   int numImages = 60000;
   int rows = 28, cols = 28;
@@ -13,6 +16,8 @@ int main(int argc, char *argv[]) {
   uint8_t *images = loadMNISTImages(imageFile, numImages, rows, cols);
   // printf("Size of images: %zu\n", sizeof(images));
   uint8_t *labels = loadMNISTLabels(labelFile, numImages);
+  uint32_t *dev_ptr;
+  cudaMalloc((void **)&dev_ptr, sizeof(images));
   // printf("Size of images: %zu\n", sizeof(labels));
   // Print first image
   printf("Label: %d\n", labels[0]);
